f6137dc91b7c8363356b07eb5edaed936d40b7ed
Siwon-Kim <siwonkim1108@gmail.com>
2023-10-23 16:10:34 -0400

    feat: sentiment analysis

--- sentiment_analysis.py
+++ sentiment_analysis.py
@@ -1,12 +1,11 @@
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.models import load_model
-import numpy as np
 import pickle, re
 from konlpy.tag import Okt
 
 okt = Okt()
 
-tokenizer_path = 'tokenizer.pkl'
+tokenizer_path = 'tokenizer.pickle'
 loaded_model = load_model('sentiment_model.h5')
 
 stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
@@ -27,7 +26,8 @@ def sentiment_predict(new_sentence):
   else:
     print("{:.2f}% 확률로 부정 코멘트 입니다.\n".format((1 - score) * 100))
 
-sentiment_predict('이 영화 개꿀잼 ㅋㅋㅋ')
-sentiment_predict('이 영화 핵노잼 ㅠㅠ')
-sentiment_predict('이딴게 영화냐 ㅉㅉ')
-sentiment_predict('감독 뭐하는 놈이냐?')
+sentiment_predict('이야.. 다른건 허술하게 만들어놓고서 미세먼지나 코로나는 어쩜저렇게 튼튼하게 만들었을까..')
+sentiment_predict('진짜 존경한다 어떻게 사람들한테 안좋은것만 저렇게 잘만들까')
+sentiment_predict('전세계를 위협중인 코로나바이러스를 퇴치하기 위해 이렇게까지 노력중인 수많은 연구진과 위료진들께 감사할 따름입니다.. 정말 고맙습니다:)')
+sentiment_predict('교수님이 이렇게 강한 코로나 바이러스는 과학 실험실안에서 만들었을 가능성이 높다고 하셨는데 그게 맞지 않을까 싶다.')
+sentiment_predict('이제 다시 힘든 상황이 올수도 있을것 같은데 모두 코로나 예방수칙 철저히 잘 지켜서 잘 이겨낼수있었으면 좋겠네요 항상 고생하시는 의료진분들께도 항상 고맙습니다. 마스크를 쓰지 않은채로 예전처럼 일상생활을 할수 있을 날이 왔으면 좋겠어요')
