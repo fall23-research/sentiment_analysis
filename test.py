4794ab63b6adeccd58c5a8505d923e436ff28976
Siwon-Kim <siwonkim1108@gmail.com>
2023-11-07 19:40:09 -0500

    feat: seoul national univ. librarty

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-from konlpy.tag import Kkma
-from konlpy.utils import pprint
-kkma = Kkma()
-pprint(kkma.sentences(u'네, 안녕하세요. 반갑습니다.'))
+from ekonlpy.sentiment import KSA
+
+text = "진짜 존경한다 어떻게 사람들한테 안좋은것만 저렇게 잘만들까"
+ksa = KSA()
+tokens = ksa.tokenize(text)
+score = ksa.get_score(tokens)
+print(score) # {'Positive': 9, 'Negative': -4, 'Polarity': 0.3846153550295881, 'Subjectivity': 0.9999999230769291}
